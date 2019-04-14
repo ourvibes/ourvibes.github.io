0c1b6b21a4a341002d874cda2f5f36d03255f5ab
ourvibes <cr1124@outlook.com>
2019-04-14 17:06:19 +0800

    Site updated: 2019-04-14 17:06:10

---
+++ js/baidu-analysis.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6e0915c481af295e4127028ad545aa71";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
